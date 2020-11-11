3d7e389c95d4f967194ce642a7c30c9abaf504e4
Carlos <carlos.trinidad@ibm.com>
2020-11-10 22:23:44 -0300

    Conectandose con Watson Assistant

--- controllers/watsonAssistantController.js
+++ controllers/watsonAssistantController.js
@@ -1,1 +1,62 @@
-const watson = require("ibm-watson");
+const AssistantV2 = require('ibm-watson/assistant/v2');
+const { IamAuthenticator } = require('ibm-watson/auth');
+
+const assistant = new AssistantV2({
+    version: process.env.WATSON_ASSISTANT_VERSION,
+    authenticator: new IamAuthenticator({
+        apikey: process.env.WATSON_ASSISTANT_APIKEY,
+    }),
+    serviceUrl: process.env.WATSON_ASSISTANT_URL,
+});
+
+let sendMessage = async (req, res) => {
+    try {
+        let assistantId = process.env.WATSON_ASSISTANT_ASSISTANT_ID;
+        let sessionId = req.body.sessionId ? req.body.sessionId : await createSession(assistantId);
+        let text = req.query.text;
+
+        /*
+        POST:
+        req.body.text
+
+        GET:
+        req.params.text (/message/:text)
+        or
+        req.query.text (/message?text='')
+        */
+
+        let payload = {
+            assistantId: assistantId,
+            sessionId: sessionId,
+            input: {
+                'message_type': 'text',
+                'text': text
+            }
+        };
+
+        console.log(payload);
+
+        let response = await assistant.message(payload);
+
+        console.log(JSON.stringify(response, null, 2));
+
+        res.status(200).send({
+            success: true,
+            sessionId: sessionId,
+            result: response.result.output.generic
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).send(error);
+    }
+}
+
+let createSession = async (assistantId) => {
+    let response = await assistant.createSession({ assistantId: assistantId });
+    console.log(`Nueva Session_ID creada: ${response.result.session_id}`);
+    return response.result.session_id;
+}
+
+module.exports = {
+    sendMessage
+}
--- routes/routes.js
+++ routes/routes.js
@@ -2,12 +2,14 @@ const express = require("express");
 const router = express.Router();
 
 const pingController = require("./../controllers/pingController");
+const watsonAssistantController = require("./../controllers/watsonAssistantController");
 
 /*
  Rutas de Ping
  */
 router.get('/ping', pingController.getPing);
 
-router.post('/watson/assistant/message', (req, res) => { res.send({ message: "watson/assistant/message" }) });
+router.post('/watson/message', watsonAssistantController.sendMessage);
+router.get('/watson/message', watsonAssistantController.sendMessage);
 
 module.exports = router;
